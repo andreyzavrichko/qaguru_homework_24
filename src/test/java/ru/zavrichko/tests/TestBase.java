604873758f8e09dae85fa8dd5a33ceb8eb7bba34
Andrey Zavrichko <andrey@zavrichko.dev>
2022-03-26 10:29:48 +0200

    add remote

--- src/test/java/ru/zavrichko/tests/TestBase.java
+++ src/test/java/ru/zavrichko/tests/TestBase.java
@@ -12,6 +12,7 @@ public class TestBase {
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://github.com";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 
     @AfterEach
